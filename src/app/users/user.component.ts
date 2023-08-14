e558d00618b660213db6ac0a4462566848b6cc63
Alejandro Roa <alejandro.roa@workiasolutions.com>
2023-08-14 14:48:39 -0400

    add unsuscribe process

--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user.interface';
 import { UserService } from './user.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   users: User[] = [];
   computedUsers: User[] = [];
   backupComputedUsers: User[] = [];
+  private userSubscription:Subscription;
   page: number = 1;
   pages: number[] = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
   ]
   sort: boolean = false;
 
-  constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService) {
+    this.userSubscription = Subscription.EMPTY;
+  }
 
   ngOnInit() {
     this.getUsers();
   }
 
   getUsers() {
-    this.userService.getUsers(this.page).subscribe((users: User[]) => {
+    this.userSubscription = this.userService.getUsers(this.page).subscribe((users: User[]) => {
       this.users = users;
       this.computedUsers = users;
     });
@@ -78,5 +82,9 @@ export class UserComponent implements OnInit {
       this.page = this.page - 1;
       this.getUsers();
     }
+  }
+  
+  ngOnDestroy(){
+    this.userSubscription.unsubscribe();
   }
 }
